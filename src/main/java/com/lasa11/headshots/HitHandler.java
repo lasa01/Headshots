0a5a256a58cf2816add50a5db878371400d2c57e
lasa11 <lassisaike11@gmail.com>
2014-05-22 22:08:13 +0300

    Removed player headshots because they didn't work

--- src/main/java/com/lasa11/headshots/HitHandler.java
+++ src/main/java/com/lasa11/headshots/HitHandler.java
@@ -3,6 +3,13 @@ package com.lasa11.headshots;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.IProjectile;
+import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.entity.boss.EntityWither;
+import net.minecraft.entity.monster.EntityMagmaCube;
+import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.passive.EntityWaterMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
@@ -15,23 +22,33 @@ public class HitHandler
 	@SubscribeEvent
 	public void onHit(LivingHurtEvent e)
 	{
+		if(e.entity instanceof EntityPlayer)return;
 		if(e.source.isProjectile())
 		{
 			Entity en = e.source.getSourceOfDamage();
 			if (e.entity instanceof EntityLiving&&en instanceof IProjectile||Headshots.EntityBullet.isInstance(en))
 			{
-				if(e.entity.posY+e.entity.getEyeHeight()-0.4F<en.posY && en.posY<e.entity.posY+e.entity.getEyeHeight()+0.4F)
+				
+				if(e.entity.posY+e.entity.getEyeHeight()-0.4F<en.posY && en.posY<e.entity.posY+e.entity.getEyeHeight()+0.4F&&!isBadEntity(e.entity))
 				{
+					System.out.println("DEBUG "+e.entity.toString());
 					e.ammount=e.ammount*5;
-					if(!(e.entity instanceof EntityPlayer) && !(e.entity instanceof EntityPlayerMP))
-					{
-						((EntityLiving) e.entityLiving).setCustomNameTag("HEADSHOT!");
-						((EntityLiving) e.entityLiving).setAlwaysRenderNameTag(true);
-					}
+					((EntityLiving) e.entityLiving).setCustomNameTag("HEADSHOT!");
+					((EntityLiving) e.entityLiving).setAlwaysRenderNameTag(true);
 				}
 				
 			}
 		}
+	}
+	
+	private boolean isBadEntity(Entity e)
+	{
+		if(e instanceof EntityVillager)return false;
+		if(e instanceof EntityAnimal)return true;
+		if(e instanceof EntityWaterMob)return true;
+		if(e instanceof EntitySlime || e instanceof EntityMagmaCube)return true;
+		if(e instanceof EntityDragon|| e instanceof EntityWither)return true; 
+		return false;
 	}
 	
 }
